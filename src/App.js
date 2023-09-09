f521522f44e90a5c37ba150be14abedffd465a87
MatheusLBrum <matheusdelimabrum@gmail.com>
2023-09-08 22:55:06 -0300

    doing the style and aplication react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [data, setData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleFileUpload = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleValidation = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await axios.post('http://localhost:3001/validate', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      setData(response.data);
+      setErrorMessage('');
+    } catch (error) {
+      console.error(error);
+      setErrorMessage('Erro ao validar o arquivo CSV.');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Validação de Arquivo CSV</h1>
+      <input type="file" accept=".csv" onChange={handleFileUpload} />
+      <button onClick={handleValidation}>VALIDAR</button>
+      {errorMessage && <p>{errorMessage}</p>}
+      {data && (
+        <table>
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Nome</th>
+              <th>Preço Atual</th>
+              <th>Novo Preço</th>
+              <th>Erros</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.code}</td>
+                <td>{item.name}</td>
+                <td>{item.currentPrice}</td>
+                <td>{item.newPrice}</td>
+                <td>{item.errors}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
